60d1dc3ca800b1ab0a36e96fc1760fe7e0635732
Michael Price <mike@quantumpolitics.org>
2011-06-15 17:47:59 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.8'
+gem 'mysql2', '~> 0.3.3'
+gem 'mysql', '~> 2.8.1'
+
+group :development do
+	gem 'rspec-rails', '2.6.1'
+	gem 'annotate-models', '1.0.4'
+end
+
+group :test do
+	gem 'rspec', '2.6.0'
+	gem 'webrat', '0.7.3'
+	gem 'spork', '0.9.0.rc8'
+	gem 'factory_girl_rails', '1.0.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '3874c1c20cd2712bd3706d55b73184e752eabe81a74e907f4148538bb645ebfdbf1bf216f140e71e3cb99f100d4152741d52155996329b1d9a26929de57a4fd5'
